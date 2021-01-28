e5cc9700ada85aa43da6f5e4fc35199f25d10486
Soumya Tripathy <st11@iitbbs.ac.in>
2021-01-28 18:26:24 +0200

    fixed the bug in total gpu count per user.
    1. now using squeue insteadof sacct
    2. support both narvi and puhti
    3. print the total gpu avialble and used in the server
    4. Display pending if a job is pending in CPU/GPU count

--- bash_user_summary.sh
+++ bash_user_summary.sh
@@ -10,32 +10,110 @@
 #CPU=`sinfo -N -p gpu -o %a -O "nodehost,available,cpusstate" --noheader | cut -d "/" -f 1 | sort -nr -k3 | tail -10`
 #echo "$CPU"
 #Total number of gpus in use
+
+
+host=`hostname -s | awk -F'[-]' '{print $1}'`
+
+if [ $host = "puhti" ]
+
+   then
 #####################################################################################
 echo
 echo '*** WELCOME TO DYNAMIC PUHTI-RESOURCE FINDER SYSTEM ***'
 echo
 #####################################################################################
 
-# HEADER 
-printf '%10s %4s %3s %7s' "USER" "CPU" "GPU" "CPU/GPU"
+# HEADER
+printf '%10s %4s %3s %12s' "USER" "CPU" "GPU" "CPU/GPU"
 echo
 
 unique_users=`squeue -h -p gpu -o "%u" | sort | uniq`
 
 for value in $unique_users
 	do
-		gpu_in_use=`sacct -n -X --state running -r gpu --user  $value --format=jobid,elapsed,ncpus,ntasks,state,AllocGRES | awk '{print $5}' | awk -F '[ :]' '{print $2}' | awk '{s+=$1} END {print s}'`
-		cpu_in_use=`sacct -n -X --state running -r gpu --user  $value --format=jobid,elapsed,ncpus,ntasks,state,AllocGRES | awk '{print $3}' | awk '{s+=$1} END {print s}'`
+	    gpu_in_use=`squeue -h -t R -p gpu --user $value -o "%.10b %10D" | awk -F'[: ]' '{print $3 * $4}' | awk '{s+=$1} END {print s}'`
+		cpu_in_use=`squeue -h -t R -p gpu --user $value -o "%.10c %10D" | awk '{print $1 * $2}' | awk '{s+=$1} END {print s}'`
 
 		# if $gpu_in_use is empty, then a user waits in the queue CPU/GPU is -1
 		if [ -z "$gpu_in_use" ]
 		then
-			cpu_per_gpu=`echo -1`
+			cpu_per_gpu=`echo PENDING`
 		else
-			cpu_per_gpu=`echo $cpu_in_use / $gpu_in_use | bc -l`
+		    if [ $gpu_in_use = 0 ]
+		    then
+			cpu_per_gpu=`echo -1`
+		    else
+			cpu_per_gpu=`echo $(printf %.1f $(echo $cpu_in_use / $gpu_in_use | bc -l))`
+		    fi
 		fi
 
 		while IFS= read -r -u2 u && read -r -u3 a && read -r -u4 b && read -r -u5 c; do
-			printf '%10s %4d %3s %7.1f\n' "$u" "$a" "$b" "$c"
+			printf '%10s %4d %3s %10s\n' "$u" "$a" "$b" "$c"
 		done 2<<<"$value" 3<<<"$cpu_in_use" 4<<<"$gpu_in_use" 5<<<"$cpu_per_gpu"
 	done
+
+echo
+echo
+
+total_gpu_in_use=`squeue -h -t R -p gpu -o "%.10b %10D" | awk -F'[: ]' '{print $3 * $4}' | paste -sd+ | bc`
+total_gpu=`sinfo -N -p gpu -o %a -O "gres" --noheader | wc -l`
+mul_fac=4
+total_gpu_in_csc=$(echo "${total_gpu}*${mul_fac}" | bc -l)
+
+echo Total GPUs in CSC: "$total_gpu_in_csc "
+
+echo Total GPUs are in use: "$total_gpu_in_use "
+
+else
+    if [ $host = 'narvi' ]
+    then
+echo
+echo '*** WELCOME TO DYNAMIC NARVI-RESOURCE FINDER SYSTEM ***'
+echo
+#####################################################################################
+
+# HEADER
+printf '%10s %4s %3s %12s' "USER" "CPU" "GPU" "CPU/GPU"
+echo
+
+unique_users=`squeue -h -p gpu -o "%u" | sort | uniq`
+
+for value in $unique_users
+	do
+	    gpu_in_use=`squeue -h -t R -p gpu --user $value -o "%.10b %10D" | awk -F'[: ]' '{print $3 * $4}' | awk '{s+=$1} END {print s}'`
+		cpu_in_use=`squeue -h -t R -p gpu --user $value -o "%.10c %10D" | awk '{print $1 * $2}' | awk '{s+=$1} END {print s}'`
+
+		# if $gpu_in_use is empty, then a user waits in the queue CPU/GPU is -1
+		if [ -z "$gpu_in_use" ]
+		then
+			cpu_per_gpu=`echo PENDING`
+		else
+		    if [ $gpu_in_use = 0 ]
+		    then
+			cpu_per_gpu=`echo -1`
+		    else
+			cpu_per_gpu=`echo $(printf %.1f $(echo $cpu_in_use / $gpu_in_use | bc -l))`
+		    fi
+		fi
+
+		while IFS= read -r -u2 u && read -r -u3 a && read -r -u4 b && read -r -u5 c; do
+			printf '%10s %4d %3s %10s\n' "$u" "$a" "$b" "$c"
+		done 2<<<"$value" 3<<<"$cpu_in_use" 4<<<"$gpu_in_use" 5<<<"$cpu_per_gpu"
+	done
+
+echo
+echo
+
+total_gpu_in_use=`squeue -h -t R -p gpu -o "%.100b %10D" | awk  '{print $1 "\t" $2 }' |sed 's/teslav100://'|sed 's/teslap100://' |sed 's/gpu://' | awk '{print $1 * $2}' | paste -sd+ | bc`
+total_gpu=`sinfo -N -p gpu -o %a -O "gres" --noheader | wc -l`
+mul_fac=4
+
+total_gpu_in_csc=$(echo "${total_gpu}*${mul_fac}" | bc -l)
+
+echo Total GPUs in CSC: "$total_gpu_in_csc "
+
+echo Total GPUs are in use: "$total_gpu_in_use "
+
+    fi
+
+fi
